475627c2cdd0968851a345a722aac4286a00529e
Chad Nelson <chad.nelson@temple.edu>
2019-08-20 13:57:07 -0400

    Fix a few prod deploy scrip issues

--- .travis/deploy-prod.sh
+++ .travis/deploy-prod.sh
@@ -2,8 +2,7 @@
 set -e
 
 LATEST_TAG=$(curl -s https://api.github.com/repos/tulibraries/funcake-solr/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-NEW_TAG=$(($LATEST_TAG + 1))
-echo $NEW_TAG
-curl -X POST --header "Content-Type:application/octet-stream" --data-binary @/home/travis/build/tulibraries/ansible-playbook-solrcloud/data/tmp/collections/funcake.zip "https://$SOLR_STAGE_USER:$SOLR_STAGE_PASSWORD@solrcloud.tul-infra.page/solr/admin/configs?action=UPLOAD&name=funcake-$NEW_TAG"
-curl "https://$SOLR_STAGE_USER:$SOLR_STAGE_PASSWORD@solrcloud.tul-infra.page/solr/admin/collections?action=CREATE&name=funcake-$NEW_TAG&numShards=1&replicationFactor=3&maxShardsPerNode=1&collection.configName=funcake-$NEW_TAG"
-curl "https://$SOLR_STAGE_USER:$SOLR_STAGE_PASSWORD@solrcloud.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=funcake-$NEW_TAG-qa&collections=funcake-$NEW_TAG"
+echo $LATEST_TAG
+curl -X POST --header "Content-Type:application/octet-stream" --data-binary @/home/travis/build/tulibraries/ansible-playbook-solrcloud/data/tmp/collections/funcake.zip "https://$SOLR_PROD_USER:$SOLR_PROD_PASSWORD@solrcloud.tul-infra.page/solr/admin/configs?action=UPLOAD&name=funcake-$LATEST_TAG"
+curl "https://$SOLR_PROD_USER:$SOLR_PROD_PASSWORD@solrcloud.tul-infra.page/solr/admin/collections?action=CREATE&name=funcake-$LATEST_TAG&numShards=1&replicationFactor=3&maxShardsPerNode=1&collection.configName=funcake-$LATEST_TAG"
+curl "https://$SOLR_PROD_USER:$SOLR_PROD_PASSWORD@solrcloud.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=funcake-$LATEST_TAG-qa&collections=funcake-$LATEST_TAG"
