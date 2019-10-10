8f7ccd72ed4a6f9fef8b26fc2e388fa660912a57
cmharlow <cmharlow@gmail.com>
2019-10-10 11:20:41 -0400

    cleaning up bash for solr creds

--- .travis/deploy-dev.sh
+++ .travis/deploy-dev.sh
@@ -4,6 +4,5 @@ set -e
 LATEST_TAG=$(curl -s https://api.github.com/repos/tulibraries/funcake-solr/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 NEW_TAG=$(($LATEST_TAG + 1))
 echo $NEW_TAG
-curl -X POST --header "Content-Type:application/octet-stream" --data-binary @/home/travis/build/tulibraries/ansible-playbook-solrcloud/data/tmp/collections/funcake.zip "https://$SOLR_STAGE_USER:$SOLR_STAGE_PASSWORD@solrcloud.tul-infra.page/solr/admin/configs?action=UPLOAD&name=funcake-$NEW_TAG-rc"
-curl "https://$SOLR_STAGE_USER:$SOLR_STAGE_PASSWORD@solrcloud.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=funcake-$NEW_TAG-rc"
-mv "/home/travis/build/tulibraries/ansible-playbook-solrcloud/data/tmp/collections/funcake.zip" "/home/travis/build/tulibraries/ansible-playbook-solrcloud/data/tmp/collections/funcake-$NEW_TAG-rc.zip"
+curl -X POST --header "Content-Type:application/octet-stream" --data-binary @/home/travis/build/tulibraries/ansible-playbook-solrcloud/data/tmp/collections/funcake.zip "https://$SOLR_USER:$SOLR_PASSWORD@solrcloud.tul-infra.page/solr/admin/configs?action=UPLOAD&name=funcake-$NEW_TAG-rc"
+curl "https://$SOLR_USER:$SOLR_PASSWORD@solrcloud.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=funcake-$NEW_TAG-rc"
