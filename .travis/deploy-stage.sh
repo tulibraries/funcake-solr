89fda18e6c4514e49e7b6f2fedf84fbbea587075
cmharlow <cmharlow@gmail.com>
2019-10-10 10:56:57 -0400

    renaming stage to dev (dpla only); adding right zipfile upload for release

--- .travis/deploy-stage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-LATEST_TAG=$(curl -s https://api.github.com/repos/tulibraries/funcake-solr/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-NEW_TAG=$(($LATEST_TAG + 1))
-echo $NEW_TAG
-curl -X POST --header "Content-Type:application/octet-stream" --data-binary @/home/travis/build/tulibraries/ansible-playbook-solrcloud/data/tmp/collections/funcake.zip "https://$SOLR_STAGE_USER:$SOLR_STAGE_PASSWORD@solrcloud.stage.tul-infra.page/solr/admin/configs?action=UPLOAD&name=funcake-$NEW_TAG"
-curl "https://$SOLR_STAGE_USER:$SOLR_STAGE_PASSWORD@solrcloud.stage.tul-infra.page/solr/admin/collections?action=CREATE&name=funcake-$NEW_TAG&numShards=1&replicationFactor=3&maxShardsPerNode=1&collection.configName=funcake-$NEW_TAG"
-curl "https://$SOLR_STAGE_USER:$SOLR_STAGE_PASSWORD@solrcloud.stage.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=funcake-$NEW_TAG&collections=funcake-$NEW_TAG"
