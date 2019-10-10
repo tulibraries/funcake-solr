74461b1f9d2fe85918353be1f328ffad9e6f4261
cmharlow <cmharlow@gmail.com>
2019-10-10 11:52:06 -0400

    aliases for first creation require a collection, blergh

--- .travis/deploy-dev.sh
+++ .travis/deploy-dev.sh
@@ -5,4 +5,5 @@ LATEST_TAG=$(curl -s https://api.github.com/repos/tulibraries/funcake-solr/relea
 NEW_TAG=$(($LATEST_TAG + 1))
 echo $NEW_TAG
 curl -X POST --header "Content-Type:application/octet-stream" --data-binary @/home/travis/build/tulibraries/ansible-playbook-solrcloud/data/tmp/collections/funcake.zip "https://$SOLR_USER:$SOLR_PASSWORD@solrcloud.tul-infra.page/solr/admin/configs?action=UPLOAD&name=funcake-$NEW_TAG-rc"
-curl "https://$SOLR_USER:$SOLR_PASSWORD@solrcloud.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=funcake-$NEW_TAG-rc"
+curl "https://$SOLR_USER:$SOLR_PASSWORD@solrcloud.tul-infra.page/solr/admin/collections?action=CREATE&name=funcake-$NEW_TAG-rc&numShards=1&replicationFactor=2&maxShardsPerNode=1&collection.configName=funcake-$NEW_TAG-rc"
+curl "https://$SOLR_USER:$SOLR_PASSWORD@solrcloud.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=funcake-$NEW_TAG-rc&collections=funcake-$NEW_TAG-rc"
