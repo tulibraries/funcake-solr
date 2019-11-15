07d7f86646ce7b55c5cef64dca6f3dcc83574679
cmharlow <cmharlow@gmail.com>
2019-11-15 17:05:44 -0500

    adding readme; fixing asset deploy to GH release

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -35,4 +35,6 @@ validate_status
 echo "***"
 echo "* Pushing zip file asset to GitHub release."
 echo "***"
-curl -v -X POST -H "Authorization: token $GITHUB_TOKEN" --data-binary @"/home/circleci/solrconfig.zip" -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/tulibraries/funcake-solr/releases/$CIRCLE_TAG/assets?name=funcake-$CIRCLE_TAG.zip"
+RELEASE_ID=$(curl "https://api.github.com/repos/tulibraries/tul_cob-az-solr/releases/latest" | jq .id)
+
+curl -v -X POST -H "Authorization: token $GITHUB_TOKEN" --data-binary @"/home/circleci/solrconfig.zip" -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/tulibraries/funcake-solr/releases/$RELEASE_ID/assets?name=funcake-$CIRCLE_TAG.zip"
